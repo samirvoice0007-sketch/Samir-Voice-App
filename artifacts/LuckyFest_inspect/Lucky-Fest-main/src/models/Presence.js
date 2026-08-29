962567045249db1036be018220f6895d9cbdf3f8
samirvoice0007-sketch <samirvoice0007@gmail.com>
2026-08-29 22:27:13 +0600

    Delete artifacts directory

--- artifacts/LuckyFest_inspect/Lucky-Fest-main/src/models/Presence.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schema = new mongoose.Schema(
-  {
-    telegramId: { type: String, unique: true, required: true },
-    lastSeenAt: { type: Date, default: Date.now, index: true }
-  },
-  { timestamps: true }
-);
-module.exports = mongoose.model("Presence", schema);
